d1fcd4c1f67ec3ec1b2a880fb66ef532ade01382
Keiko Tully <keikotully0@gmail.com>
2023-03-19 20:15:40 -0500

    Added sticky nav, flex and flex reverse

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// reference: https://www.youtube.com/watch?v=axQ7eN2oPu0
+
+
+
+const headerE1 = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        headerE1.classList.add('header-scrolled');
+    } else if (window.scrollY <= 50){
+        headerE1.classList.remove('header-scrolled');
+    }
+});
